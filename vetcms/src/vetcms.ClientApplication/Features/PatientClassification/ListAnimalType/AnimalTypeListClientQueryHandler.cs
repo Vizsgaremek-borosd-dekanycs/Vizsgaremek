282df307c097eed71a25637bddbd838a5d2e1848
rizzilla <141221167+rizzilla@users.noreply.github.com>
2025-02-24 17:38:20 +0100

    minor fixes

--- vetcms/src/vetcms.ClientApplication/Features/PatientClassification/ListAnimalType/AnimalTypeListClientQueryHandler.cs
+++ vetcms/src/vetcms.ClientApplication/Features/PatientClassification/ListAnimalType/AnimalTypeListClientQueryHandler.cs
@@ -9,7 +9,7 @@ using vetcms.SharedModels.Common.Dto;
 
 namespace vetcms.ClientApplication.Features.PatientClassification.ListAnimalType
 {
-    internal class AnimalTypeListClientQueryHandler(IMediator mediator) : IRequestHandler<AnimalTypeListClientQuery, AnimalTypeByBreedListClientQueryResponse>
+    internal class AnimalTypeListClientQueryHandler(IMediator mediator) : IRequestHandler<AnimalTypeListClientQuery, AnimalTypeListClientQueryResponse>
     {
         private static readonly List<AnimalTypeDto> AnimalTypes = new()
         {
@@ -18,23 +18,23 @@ namespace vetcms.ClientApplication.Features.PatientClassification.ListAnimalType
             new AnimalTypeDto { Id = 3, TypeName = "Bird", Description = "Various species of birds" },
             new AnimalTypeDto { Id = 4, TypeName = "Fish", Description = "Various species of fish" },
             new AnimalTypeDto { Id = 5, TypeName = "Reptile", Description = "Various species of reptiles" },
-            new AnimalTypeDto { Id = 1, TypeName = "Dog", Description = "Domestic dog" },
-            new AnimalTypeDto { Id = 2, TypeName = "Cat", Description = "Domestic cat" },
-            new AnimalTypeDto { Id = 3, TypeName = "Bird", Description = "Various species of birds" },
-            new AnimalTypeDto { Id = 4, TypeName = "Fish", Description = "Various species of fish" },
-            new AnimalTypeDto { Id = 5, TypeName = "Reptile", Description = "Various species of reptiles" },
-            new AnimalTypeDto { Id = 1, TypeName = "Dog", Description = "Domestic dog" },
-            new AnimalTypeDto { Id = 2, TypeName = "Cat", Description = "Domestic cat" },
-            new AnimalTypeDto { Id = 3, TypeName = "Bird", Description = "Various species of birds" },
-            new AnimalTypeDto { Id = 4, TypeName = "Fish", Description = "Various species of fish" },
-            new AnimalTypeDto { Id = 5, TypeName = "Reptile", Description = "Various species of reptiles" },
-            new AnimalTypeDto { Id = 1, TypeName = "Dog", Description = "Domestic dog" },
-            new AnimalTypeDto { Id = 2, TypeName = "Cat", Description = "Domestic cat" },
-            new AnimalTypeDto { Id = 3, TypeName = "Bird", Description = "Various species of birds" },
-            new AnimalTypeDto { Id = 4, TypeName = "Fish", Description = "Various species of fish" },
-            new AnimalTypeDto { Id = 5, TypeName = "Reptile", Description = "Various species of reptiles" }
+            new AnimalTypeDto { Id = 6, TypeName = "Dog", Description = "Domestic dog" },
+            new AnimalTypeDto { Id = 7, TypeName = "Cat", Description = "Domestic cat" },
+            new AnimalTypeDto { Id = 8, TypeName = "Bird", Description = "Various species of birds" },
+            new AnimalTypeDto { Id = 9, TypeName = "Fish", Description = "Various species of fish" },
+            new AnimalTypeDto { Id = 10, TypeName = "Reptile", Description = "Various species of reptiles" },
+            new AnimalTypeDto { Id = 11, TypeName = "Dog", Description = "Domestic dog" },
+            new AnimalTypeDto { Id = 12, TypeName = "Cat", Description = "Domestic cat" },
+            new AnimalTypeDto { Id = 13, TypeName = "Bird", Description = "Various species of birds" },
+            new AnimalTypeDto { Id = 14, TypeName = "Fish", Description = "Various species of fish" },
+            new AnimalTypeDto { Id = 15, TypeName = "Reptile", Description = "Various species of reptiles" },
+            new AnimalTypeDto { Id = 16, TypeName = "Dog", Description = "Domestic dog" },
+            new AnimalTypeDto { Id = 17, TypeName = "Cat", Description = "Domestic cat" },
+            new AnimalTypeDto { Id = 18, TypeName = "Bird", Description = "Various species of birds" },
+            new AnimalTypeDto { Id = 19, TypeName = "Fish", Description = "Various species of fish" },
+            new AnimalTypeDto { Id = 20, TypeName = "Reptile", Description = "Various species of reptiles" }
         };
-        public Task<AnimalTypeByBreedListClientQueryResponse> Handle(AnimalTypeListClientQuery request, CancellationToken cancellationToken)
+        public Task<AnimalTypeListClientQueryResponse> Handle(AnimalTypeListClientQuery request, CancellationToken cancellationToken)
         {
             var filteredAnimalTypes = AnimalTypes;
 
@@ -47,7 +47,7 @@ namespace vetcms.ClientApplication.Features.PatientClassification.ListAnimalType
                 .Take(request.Take ?? filteredAnimalTypes.Count)
                 .ToList();
 
-            var response = new AnimalTypeByBreedListClientQueryResponse
+            var response = new AnimalTypeListClientQueryResponse
             {
                 AnimalType = paginatedAnimalTypes,
                 ResultCount = filteredAnimalTypes.Count
