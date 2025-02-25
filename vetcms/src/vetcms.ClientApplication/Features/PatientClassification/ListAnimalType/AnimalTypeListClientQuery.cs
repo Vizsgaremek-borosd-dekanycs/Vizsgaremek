64405174f74b60e4adf663035c93849a1cda54c2
rizzilla <141221167+rizzilla@users.noreply.github.com>
2025-02-25 22:41:48 +0100

    minor changes

--- vetcms/src/vetcms.ClientApplication/Features/PatientClassification/ListAnimalType/AnimalTypeListClientQuery.cs
+++ vetcms/src/vetcms.ClientApplication/Features/PatientClassification/ListAnimalType/AnimalTypeListClientQuery.cs
@@ -10,7 +10,6 @@ namespace vetcms.ClientApplication.Features.PatientClassification.ListAnimalType
 {
     public class AnimalTypeListClientQuery : IClientCommand<AnimalTypeListClientQueryResponse>
     {
-        public int BreedId { get; set; } = 0;
         public string SearchTerm { get; set; } = string.Empty;
         public int Skip { get; set; } = 0;
         public int? Take { get; set; }
