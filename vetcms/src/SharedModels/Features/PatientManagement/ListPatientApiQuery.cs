963afc4a6a76c571628a0fa673a48a06fc0a365a
rabszolgamunkás <borosdani47@gmail.com>
2025-03-12 17:17:11 +0100

    list api queries

---
+++ vetcms/src/SharedModels/Features/PatientManagement/ListPatientApiQuery.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.SharedModels.Common;
+using vetcms.SharedModels.Common.Abstract;
+using vetcms.SharedModels.Common.Dto;
+using vetcms.SharedModels.Common.IAM.Authorization;
+using vetcms.SharedModels.Features.PatientClassification;
+
+namespace vetcms.SharedModels.Features.PatientManagement
+{
+    public record ListPatientApiQuery : AuthenticatedApiCommandBase<ListPatientApiQueryResponse>
+    {
+        public int Skip { get; set; } = 0;
+        public int Take { get; set; } = 10;
+        public string SearchTerm { get; set; } = string.Empty;
+        public override string GetApiEndpoint()
+        {
+            return $"/api/v1/animal-management/animals?skip={Skip}&take={Take}&query={SearchTerm}";
+        }
+
+        public override HttpMethodEnum GetApiMethod()
+        {
+            return HttpMethodEnum.Get;
+        }
+
+        public override PermissionFlags[] GetRequiredPermissions()
+        {
+            return [PermissionFlags.CAN_VIEW_PATIENTS];
+        }
+    }
+
+
+    public class ListPatientApiQueryValidator : AbstractValidator<ListPatientApiQuery>
+    {
+        public ListPatientApiQueryValidator()
+        {
+            RuleFor(x => x.Skip)
+                .GreaterThanOrEqualTo(0).WithMessage("A Skip értéke nem lehet kisebb, mint 0");
+
+            RuleFor(x => x.Take)
+                .GreaterThan(0).WithMessage("A Take értéke nagyobb kell legyen, mint 0")
+                .LessThanOrEqualTo(100).WithMessage("A Take értéke nem lehet nagyobb, mint 100");
+
+            RuleFor(x => x.SearchTerm)
+                .MaximumLength(100).WithMessage("A keresési kifejezés nem haladhatja meg a 100 karaktert");
+        }
+    }
+
+    public record ListPatientApiQueryResponse : AuthenticatedCommandResult
+    {
+        public int ResoultCount { get; set; } = 0;
+        public List<PatientDto> Patients { get; set; } = new List<PatientDto>();
+        public ListPatientApiQueryResponse()
+        {
+
+        }
+
+        public ListPatientApiQueryResponse(bool success, string message = "")
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
---
+++ vetcms/src/SharedModels/Features/PatientManagement/ListPatientTreatmentApiQuery.cs
@@ -0,0 +1,69 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.SharedModels.Common;
+using vetcms.SharedModels.Common.Abstract;
+using vetcms.SharedModels.Common.Dto;
+using vetcms.SharedModels.Common.IAM.Authorization;
+
+namespace vetcms.SharedModels.Features.PatientManagement
+{
+    public record ListPatientTreatmentApiQuery : AuthenticatedApiCommandBase<ListPatientTreatmentApiQueryResponse>
+    {
+        public int Skip { get; set; } = 0;
+        public int Take { get; set; } = 10;
+        public string SearchTerm { get; set; } = string.Empty;
+
+        public override string GetApiEndpoint()
+        {
+            return $"/api/v1/animal-management/treatments?skip={Skip}&take={Take}&query={SearchTerm}";
+        }
+
+        public override HttpMethodEnum GetApiMethod()
+        {
+            return HttpMethodEnum.Get;
+        }
+
+        public override PermissionFlags[] GetRequiredPermissions()
+        {
+            return [PermissionFlags.CAN_VIEW_PATIENT_TREATMENTS];
+        }
+    }
+
+    public class ListPatientTreatmentApiQueryValidator : AbstractValidator<ListPatientTreatmentApiQuery>
+    {
+        public ListPatientTreatmentApiQueryValidator()
+        {
+            RuleFor(x => x.Skip)
+                .GreaterThanOrEqualTo(0).WithMessage("A Skip értéke nem lehet kisebb, mint 0");
+
+            RuleFor(x => x.Take)
+                .GreaterThan(0).WithMessage("A Take értéke nagyobb kell legyen, mint 0")
+                .LessThanOrEqualTo(100).WithMessage("A Take értéke nem lehet nagyobb, mint 100");
+
+            RuleFor(x => x.SearchTerm)
+                .MaximumLength(100).WithMessage("A keresési kifejezés nem haladhatja meg a 100 karaktert");
+        }
+    }
+
+
+    public record ListPatientTreatmentApiQueryResponse : AuthenticatedCommandResult
+    {
+        public int ResoultCount { get; set; } = 0;
+        public List<TreatmentDto> Treatments { get; set; } = new List<TreatmentDto>();
+        public ListPatientTreatmentApiQueryResponse()
+        {
+
+        }
+
+        public ListPatientTreatmentApiQueryResponse(bool success, string message = "")
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+
+}
