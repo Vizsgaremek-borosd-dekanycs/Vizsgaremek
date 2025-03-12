6036d53709dd1e01755e93d6c8db997390a2cc26
rabszolgamunkás <borosdani47@gmail.com>
2025-03-12 16:34:05 +0100

    Create entities

---
+++ vetcms/src/SharedModels/Common/Dto/TreatmentDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vetcms.SharedModels.Common.Dto
+{
+    public class TreatmentDto
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime DateOfTreatment { get; set; }
+        public string Type { get; set; }
+        public string? Symptoms { get; set; }
+        public string? Diagnosis { get; set; }
+        public string? Medications { get; set; }
+        public string? Procedure { get; set; }
+        public string? Description { get; set; }
+        public DateTime? FollowUpDate { get; set; }
+    }
+}
---
+++ vetcms/src/vetcms.ServerApplication/Domain/Entity/PatientManagement/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.ServerApplication.Common.Abstractions.Data;
+
+namespace vetcms.ServerApplication.Domain.Entity
+{
+    public class Patient : AuditedEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public char Gender { get; set; }
+        public double Weight { get; set; }
+        public string MicrochipNumber { get; set; }
+        public bool IsSterilised { get; set; }
+        public string ChronicDiseases { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public User Owner { get; set; }
+        public AnimalType Type { get; set; }
+        public AnimalBreed Breed { get; set; }
+    }
+}
