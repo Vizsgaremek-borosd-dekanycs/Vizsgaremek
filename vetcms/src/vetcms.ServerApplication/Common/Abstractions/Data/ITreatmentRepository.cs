f89f97e45bbe3586b8ab7fa967ad966cabfdf4c4
rabszolgamunkás <borosdani47@gmail.com>
2025-03-13 15:52:47 +0100

    wip

---
+++ vetcms/src/vetcms.ServerApplication/Common/Abstractions/Data/ITreatmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.ServerApplication.Domain.Entity.PatientManagement;
+
+namespace vetcms.ServerApplication.Common.Abstractions.Data
+{
+    public interface ITreatmentRepository : IRepositoryBase<Treatment>
+    {
+    }
+}
---
+++ vetcms/src/vetcms.ServerApplication/Infrastructure/Presistence/Repository/PatientRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.ServerApplication.Common.Abstractions.Data;
+using vetcms.ServerApplication.Domain.Entity.PatientManagement;
+
+namespace vetcms.ServerApplication.Infrastructure.Presistence.Repository
+{
+    internal class PatientRepository : RepositoryBase<Patient>, IAnimalBreedRepository
+    {
+        public PatientRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
