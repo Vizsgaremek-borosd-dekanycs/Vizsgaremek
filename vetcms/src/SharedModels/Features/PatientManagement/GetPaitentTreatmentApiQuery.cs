45c97df00e29ea68dd38f3cd0636520154dfc28e
rabszolgamunkás <borosdani47@gmail.com>
2025-03-13 14:51:00 +0100

    Get api queries

---
+++ vetcms/src/SharedModels/Features/PatientManagement/GetPaitentTreatmentApiQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.SharedModels.Common;
+using vetcms.SharedModels.Common.Abstract;
+using vetcms.SharedModels.Common.Dto;
+using vetcms.SharedModels.Common.IAM.Authorization;
+
+namespace vetcms.SharedModels.Features.PatientManagement
+{
+    public record GetPaitentTreatmentApiQuery : AuthenticatedApiCommandBase<GetPaitentTreatmentApiQueryResponse>
+    {
+        public int TreatmentId { get; set; }
+        public override string GetApiEndpoint()
+        {
+            return $"/api/v1/animal-management/treatments/{TreatmentId}";
+        }
+
+        public override HttpMethodEnum GetApiMethod()
+        {
+            return HttpMethodEnum.Get;
+        }
+
+        public override PermissionFlags[] GetRequiredPermissions()
+        {
+            return [PermissionFlags.CAN_VIEW_PATIENT_TREATMENTS];
+        }
+    }
+
+
+    public record GetPaitentTreatmentApiQueryResponse : AuthenticatedCommandResult
+    {
+        public TreatmentDto TreatmentModel { get; set; }
+        public GetPaitentTreatmentApiQueryResponse()
+        {
+            
+        }
+
+        public GetPaitentTreatmentApiQueryResponse(bool _success, string _message = "")
+        {
+            Success = _success;
+            Message = _message;
+        }
+    }
+}
---
+++ vetcms/src/SharedModels/Features/PatientManagement/GetPatientApiQuery.cs
@@ -0,0 +1,62 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.SharedModels.Common;
+using vetcms.SharedModels.Common.Abstract;
+using vetcms.SharedModels.Common.Dto;
+using vetcms.SharedModels.Common.IAM.Authorization;
+using vetcms.SharedModels.Features.PatientClassification;
+
+namespace vetcms.SharedModels.Features.PatientManagement
+{
+    public record GetPatientApiQuery : AuthenticatedApiCommandBase<GetPatientApiQueryResponse>
+    {
+        public int PatientId { get; set; } = 0;
+
+        public override string GetApiEndpoint()
+        {
+            return $"/api/v1/animal-management/animals/{PatientId}";
+        }
+
+        public override HttpMethodEnum GetApiMethod()
+        {
+            return HttpMethodEnum.Get;
+        }
+
+        public override PermissionFlags[] GetRequiredPermissions()
+        {
+            return [PermissionFlags.CAN_VIEW_PATIENTS];
+        }
+    }
+
+    public class GetPatientApiQueryValidator : AbstractValidator<GetPatientApiQuery>
+    {
+        public GetPatientApiQueryValidator()
+        {
+            RuleFor(x => x.PatientId)
+                .GreaterThan(0)
+                .WithMessage("Az azonosítónak nagyobbnak kell lennie 0-nál.");
+        }
+    }
+
+
+    public record GetPatientApiQueryResponse : AuthenticatedCommandResult
+    {
+        public PatientDto PatientModel { get; set; }
+
+        public GetPatientApiQueryResponse()
+        {
+
+        }
+
+        public GetPatientApiQueryResponse(bool success, string message = "")
+        {
+            Success = success;
+            Message = message;
+        }
+
+    }
+}
