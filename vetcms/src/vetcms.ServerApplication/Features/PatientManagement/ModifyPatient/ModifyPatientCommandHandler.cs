8f626636322abfe46e882fbc5b7e19d75438dd70
rizzilla <141221167+rizzilla@users.noreply.github.com>
2025-03-15 13:50:39 +0100

    modify api command handlers

---
+++ vetcms/src/vetcms.ServerApplication/Features/PatientManagement/ModifyPatient/ModifyPatientCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.ServerApplication.Common.Abstractions.Data;
+using vetcms.ServerApplication.Domain.Entity;
+using vetcms.ServerApplication.Domain.Entity.PatientManagement;
+using vetcms.ServerApplication.Infrastructure.Presistence.Repository;
+using vetcms.SharedModels.Common.Dto;
+using vetcms.SharedModels.Features.PatientClassification;
+using vetcms.SharedModels.Features.PatientManagement;
+
+namespace vetcms.ServerApplication.Features.PatientManagement.ModifyPatient
+{
+    internal class ModifyPatientCommandHandler(IMapper mapper, IPatientRepository patientRepository) : IRequestHandler<ModifyPatientApiCommand, ModifyPatientApiCommandResponse>
+    {
+        public async Task<ModifyPatientApiCommandResponse> Handle(ModifyPatientApiCommand request, CancellationToken cancellationToken)
+        {
+            if (!await patientRepository.ExistAsync(request.Id))
+            {
+                return new ModifyPatientApiCommandResponse
+                {
+                    Success = false,
+                    Message = "Nem létezik ilyen bejegyzés az adatbázisban."
+                };
+            }
+
+            Patient updatedPatient = mapper.Map<Patient>(request.PatientModel);
+            updatedPatient.Id = request.Id;
+            await patientRepository.UpdateAsync(updatedPatient);
+
+            return new ModifyPatientApiCommandResponse(true);
+        }
+    }
+}
---
+++ vetcms/src/vetcms.ServerApplication/Features/PatientManagement/ModifyTreatment/ModifyPatientTreatmentCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.ServerApplication.Common.Abstractions.Data;
+using vetcms.ServerApplication.Domain.Entity.PatientManagement;
+using vetcms.ServerApplication.Infrastructure.Presistence.Repository;
+using vetcms.SharedModels.Features.PatientManagement;
+
+namespace vetcms.ServerApplication.Features.PatientManagement.ModifyTreatment
+{
+    internal class ModifyPatientTreatmentCommandHandler(IMapper mapper, ITreatmentRepository treatmentRepository) : IRequestHandler<ModifyPatientTreatmentApiCommand, ModifyPatientTreatmentApiCommandResponse>
+    {
+        public async Task<ModifyPatientTreatmentApiCommandResponse> Handle(ModifyPatientTreatmentApiCommand request, CancellationToken cancellationToken)
+        {
+            if (!await treatmentRepository.ExistAsync(request.Id))
+            {
+                return new ModifyPatientTreatmentApiCommandResponse
+                {
+                    Success = false,
+                    Message = "Nem létezik ilyen bejegyzés az adatbázisban."
+                };
+            }
+
+            Treatment updatedTreatment = mapper.Map<Treatment>(request.TreatmentModel);
+            updatedTreatment.Id = request.Id;
+            await treatmentRepository.UpdateAsync(updatedTreatment);
+
+            return new ModifyPatientTreatmentApiCommandResponse(true);
+        }
+    }
+}
